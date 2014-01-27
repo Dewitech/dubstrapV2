1bd115ab10c8ee4bb53ec136a276db03a8a4edc7
ryanhidajat <insomni4k@yahoo.com>
2014-01-27 15:54:11 +0700

    home prettyphoto fix

--- index.php
+++ index.php
@@ -37,26 +37,19 @@ get_header();
 	  
 	  <!-- Featured Portfolio Start -->
 	  <div class="row">
-		<?php query_posts('category_name='. get_option('dt_fcat2')); ?>
+		<?php query_posts('category_name='. get_option('dt_fcat2') .'&posts_per_page='. get_option('dt_fcatnum2') .''); ?>
 		
 		  <?php while (have_posts()) : the_post(); ?>
-			<?php
-				$args = array('post_type' => 'attachment', 'post_parent' => $post->ID,  'orderby' => menu_order, 'order' => ASC, 'posts_per_page' => get_option('dt_fcatnum2')); 
-				$attachments = get_children($args); 							
+		  <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID, 'thumbnail') ); ?>
 
-				foreach ($attachments as $attachment) { 
-					$full_url = wp_get_attachment_url($attachment->ID);
-					$image_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
-					$full = wp_get_attachment_image_src($attachment->ID, 'full');
-					$alt = get_post_meta($attachment->ID, '_wp_attachment_image_alt', true);
-					$image_title = $attachment->post_title;
-			?>
-				<div class="col-md-4 col-sm-6 portfolio-img">
-					<a href="<?php echo $full_url;?>" data-rel="prettyPhoto" rel="prettyPhoto">
-						<img src="<?php echo $full_url; ?>" title="<?php the_title(); ?>" id="homethumb" class="img-responsive" alt="<?php the_title(); ?>" />
+				<div class="col-md-4 col-sm-6">
+					<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php echo the_title(); ?></a></h2>
+					<a href="<?php echo $url;?>" data-rel="prettyPhoto" rel="prettyPhoto">
+						<img src="<?php echo $url ?>" title="	<?php the_title(); ?>" id="homethumb" class="img-responsive" alt="<?php the_title(); ?>" />
 					</a>
+					<?php the_excerpt(); ?>
+					<p><a class="btn btn-default" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">View details &raquo;</a></p>
 				</div>
-			<?php } ?>
 		  <?php endwhile;?>
 		  <?php wp_reset_query(); ?>
 	  </div>
